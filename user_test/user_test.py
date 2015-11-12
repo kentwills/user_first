04143726702fbc050ef1108bbc423a40c0a7ab06
Kent Wills <ronald.k.wills@gmail.com>
2015-11-12 13:11:54 -0800

    Models

--- user_test/user_test.py
+++ user_test/user_test.py
@@ -64,6 +64,37 @@ class Greeting(ndb.Model):
     content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
+
+class Attributes(ndb.Model):
+    attribute = ndb.StringProperty()
+
+
+class User(ndb.Model):
+    """Model for our Users"""
+    token = ndb.IntegerProperty()
+    admin = ndb.IntegerProperty()
+    attributes = ndb.KeyProperty(kind="Attributes")
+
+
+class Project(ndb.Model):
+    """PM projects"""
+    owner = ndb.KeyProperty(kind="User")
+    description = ndb.StringProperty()
+    date_time = ndb.DateTimeProperty()
+    location = ndb.StringProperty()
+    status = ndb.IntegerProperty()
+
+
+class ProjectUsers(ndb.Model):
+    project = ndb.KeyProperty(kind="Project")
+    user = ndb.KeyProperty(kind="User")
+    status = ndb.IntegerProperty()
+
+
+class Team(ndb.Model):
+    type = ndb.StringProperty()
+
+
 @app.route('/')
 def main_page():
     """
