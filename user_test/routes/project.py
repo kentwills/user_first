21f9d967c3f7dd14fedc28c15def5eb86027ca82
Kent Wills <ronald.k.wills@gmail.com>
2015-11-13 08:07:30 -0800

    link project page and fix formatting

--- user_test/routes/project.py
+++ user_test/routes/project.py
@@ -6,5 +6,5 @@ project = Blueprint('project', __name__, template_folder='templates')
 
 @project.route('/project')
 def main():
-    projects = models.Project.query()
-    return render_template('project.html', team_list=projects)
+    project_details = models.Project.query()
+    return render_template('project.html', project_details=project_details)
