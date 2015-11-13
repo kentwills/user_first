ab6a03fa3aaec1a0fc3483398f386e5aba6fd48f
Kent Wills <ronald.k.wills@gmail.com>
2015-11-13 08:57:43 -0800

    tie in project data to page

--- user_test/routes/project.py
+++ user_test/routes/project.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint
+from flask import render_template
+from flask import request
 import models
+from google.appengine.ext import ndb
 
 project = Blueprint('project', __name__, template_folder='templates')
 
 
-@project.route('/project')
-def main():
-    project_details = models.Project.query()
+@project.route('/project/<int:project_id>')
+def main(project_id):
+    project_details = ndb.Key('Project', project_id).get()
     return render_template('project.html', project_details=project_details)
