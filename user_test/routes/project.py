f3ed304e2c91abe4a3d7e8f874f72f60ff2b077c
Jonathan Maltz <maltz@yelp.com>
2015-11-13 14:14:04 -0800

    Redirect user to project_owner if they are the owner of the project they are viewing.

--- user_test/routes/project.py
+++ user_test/routes/project.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 from flask import session
+from flask import redirect
 import models
 from google.appengine.ext import ndb
 from models import User
@@ -17,10 +18,14 @@ project = Blueprint('project', __name__, template_folder='templates')
 @login_required
 def main(project_id):
     project_details = ndb.Key('Project', project_id).get()
-
+    # If the user is the owner of this event, then redirect to the owner page.
+    if session['user_id'] == project_details.owner.id():
+        return redirect('/project_owner/' + str(project_id))
+    
     return render_template(
         'project.html',
         project_details=project_details,
         user_photo_url=session['photo_url'],
         participants=[User(first_name="Kent", last_name="Wills", team=Team.query(Team.type == 'Yelp Consumer').get().key)]
     )
+
