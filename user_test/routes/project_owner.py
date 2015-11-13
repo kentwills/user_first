879d83eef574d072923e9ef8c898e17723e688d7
Kent Wills <ronald.k.wills@gmail.com>
2015-11-13 10:37:08 -0800

    project_owner page

---
+++ user_test/routes/project_owner.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from flask import render_template
+from flask import request
+import models
+from google.appengine.ext import ndb
+from models import User
+from models import Team
+
+project_owner = Blueprint('project_owner', __name__, template_folder='templates')
+
+
+@project_owner.route('/project_owner/<int:project_id>')
+def main(project_id):
+    project_details = ndb.Key('Project', project_id).get()
+    return render_template(
+        'project_owner.html',
+        project_details=project_details,
+        attributes={"age": "31", "sex": "male"},
+        participants=[User(first_name="Kent", last_name="Wills", team=Team.query(Team.type == 'Yelp-Consumer').get().key)]
+    )
--- user_test/user_test.py
+++ user_test/user_test.py
@@ -5,6 +5,7 @@ from flask import Flask
 
 from routes.main import main
 from routes.project import project
+from routes.project_owner import project_owner
 from routes.projects import projects
 from routes.login import login
 from routes.create_fake_project import create_fake_project
@@ -18,6 +19,7 @@ app = Flask(__name__)
 app.register_blueprint(main)
 app.register_blueprint(create_fake_project)
 app.register_blueprint(project)
+app.register_blueprint(project_owner)
 app.register_blueprint(projects)
 app.register_blueprint(goog_auth_response)
 app.register_blueprint(login)
