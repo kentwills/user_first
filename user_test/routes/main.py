d8f8139eed62bd501d9e2470dbb6c50c502e3e7f
Kent Wills <ronald.k.wills@gmail.com>
2015-11-12 20:49:54 -0800

    delete unused items

--- user_test/routes/main.py
+++ user_test/routes/main.py
@@ -1,40 +1,21 @@
-
-import urllib
-
 import flask
-from flask import request
-from models import Author
-from models import Greeting
-from flask import redirect
-from google.appengine.api import users
-from google.appengine.ext import ndb
 from gplus_oauth import CLIENT_SECRETS
 from oauth2client import client
 
-#from gplus_oauth import service
+from flask import Blueprint, render_template
+
 
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
 
-from flask import Blueprint, render_template, abort, redirect
-
 main = Blueprint('main', __name__, template_folder='templates')
 
-#@oauth2_decorator.oauth_required
+
 @main.route('/')
 def main_page():
-    #return redirect(flow.step1_get_authorize_url())
     flow = client.flow_from_clientsecrets(
         CLIENT_SECRETS,
         scope='https://www.googleapis.com/auth/plus.me',
-        redirect_uri= flask.url_for('goog_auth_response.goog_auth_response_page', _external=True),
+        redirect_uri = flask.url_for('goog_auth_response.goog_auth_response_page', _external=True),
     )
     return render_template('welcome.html', auth_url=flow.step1_get_authorize_url())
-
-
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity.
-
-    We use guestbook_name as the key.
-    """
-    return ndb.Key('Guestbook', guestbook_name)
--- user_test/user_test.py
+++ user_test/user_test.py
@@ -3,20 +3,17 @@ import sys
 
 from flask import Flask
 
-from routes.sign import sign
 from routes.main import main
 from routes.project import project
 from routes.projects import projects
 from routes.login import login
 from routes.goog_auth_response import goog_auth_response
-#from gplus_oauth import oauth2_decorator
+
 
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
 
-
 app = Flask(__name__)
 app.register_blueprint(main)
-app.register_blueprint(sign)
 app.register_blueprint(project)
 app.register_blueprint(projects)
 app.register_blueprint(goog_auth_response)
