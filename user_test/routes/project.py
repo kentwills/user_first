04f182f237a0957750fc8675967f4c9caf6b8fbb
Jonathan Maltz <maltz@yelp.com>
2015-11-13 15:48:44 -0800

    Create the basics of participation

--- user_test/routes/project.py
+++ user_test/routes/project.py
@@ -25,7 +25,30 @@ def main(project_id):
     return render_template(
         'project.html',
         project_details=project_details,
+        project_id=project_id,
         user_photo_url=session['photo_url'],
         participants=[User(first_name="Kent", last_name="Wills", team=Team.query(Team.type == 'Yelp Consumer').get().key)]
     )
 
+@project.route('/project/<int:project_id>/participate', methods=['POST'])
+@login_required
+def participate(project_id):
+    project_users = ProjectUsers(project=project_id, status=model.STATUS_ACTIVE).fetch()
+    current_user_id = session['user_id']
+    project_record = None
+
+    for record in project_users:
+        if record.user.id() == current_user_id:
+            project_record = record
+            break
+
+    if project_record is None:
+        # User is not already participating, add that record to the database
+        ProjectUsers(user=nbd.Key(models.User, current_user_id),
+                project=project_id, status=model.STATUS_ACTIVE).put()
+    else:
+        # User is participating, issue a delete.
+        ProjectUsers(user=current_user.key.id(),
+                project=project_id, status=model.STATUS_DEACTIVE).put()
+
+    return redirect('/project/' + str(project_id))
