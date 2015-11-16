944bb913984254c3bc284f39bffb86bc5ff9dc6a
Kent Wills <ronald.k.wills@gmail.com>
2015-11-15 17:35:10 -0800

    don't show non-active projects

--- user_test/routes/projects.py
+++ user_test/routes/projects.py
@@ -12,45 +12,48 @@ from route_utils import login_required
 projects = Blueprint('projects', __name__, template_folder='templates')
 
 
-@projects.route('/projects', methods=["GET", "POST"])
+@projects.route('/projects', methods=["GET"])
 @login_required
 def main():
-    if request.method == 'POST':
-        project_name = request.form['project_name']
-        project_description = request.form['description']
-        location = request.form['location']
-        room_name = request.form['room_name']
-        product_type = request.form['product_type']
-        time = request.form['time_frame']
-        date = request.form['date']
-        qualifications = request.form['qualifications']
+    project_list = models.Project.query(models.Project.status == models.STATUS_ACTIVE).fetch()
+    team_list = models.Team.query().fetch()
 
-        project = models.Project(
-                owner=ndb.Key(models.User, session['user_id']),
-                team=ndb.Key(models.Team, int(product_type)),
-                title=project_name,
-                description=project_description,
-                date=date,
-                time_range=time,
-                location=location,
-                room_name=room_name,
-                status=models.STATUS_ACTIVE,
-                qualifications=qualifications
-                ).put()
+    teams = {}
+    for project in project_list:
+        teams[project.team.id()] = models.Team.get_by_id(int(project.team.id()))
 
-        return redirect('/project_owner/' + str(project.id()))
-    else:
-        project_list = models.Project.query().fetch()
-        team_list = models.Team.query().fetch()
+    return render_template(
+        'projects.html',
+        project_list=project_list,
+        team_list=team_list,
+        teams=teams,
+        user_photo_url=session['photo_url'],
+    )
 
-        teams = {}
-        for project in project_list:
-            teams[project.team.id()] = models.Team.get_by_id(int(project.team.id()))
 
-        return render_template(
-            'projects.html',
-            project_list=project_list,
-            team_list=team_list,
-            teams=teams,
-            user_photo_url=session['photo_url'],
-        )
+@projects.route('/projects', methods=["POST"])
+@login_required
+def create_project():
+    project_name = request.form['project_name']
+    project_description = request.form['description']
+    location = request.form['location']
+    room_name = request.form['room_name']
+    product_type = request.form['product_type']
+    time = request.form['time_frame']
+    date = request.form['date']
+    qualifications = request.form['qualifications']
+
+    project = models.Project(
+            owner=ndb.Key(models.User, session['user_id']),
+            team=ndb.Key(models.Team, int(product_type)),
+            title=project_name,
+            description=project_description,
+            date=date,
+            time_range=time,
+            location=location,
+            room_name=room_name,
+            status=models.STATUS_ACTIVE,
+            qualifications=qualifications
+            ).put()
+
+    return redirect('/project_owner/' + str(project.id()))
